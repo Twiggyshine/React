e2b6c3772d1950783e8fbafcdcb96777ce0a23ee
Olga <olga_bezrukova@bk.ru>
2024-03-18 18:32:26 +0300

    react file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import classnames from 'classnames';
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,10 +8,13 @@ const App = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+  const classNames = classnames('burger-menu', {
+    'open': isOpen,
+  });
 
   return (
     <div>
-      <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+      <div className={classNames} onClick={toggleMenu}>
         <div className="lane"></div>
         <div className="lane"></div>
         <div className="lane"></div>
